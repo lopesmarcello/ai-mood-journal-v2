0ffb76e57ec2edc5a7404d260a54e5b2e2d504da
Marcello Lopes <marcellolopesdev@gmail.com>
2026-01-17 23:01:28 -0300

    auth

---
+++ backend/dto/register.go
@@ -0,0 +1,7 @@
+package dto
+
+type RegisterRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+}
